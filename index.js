336a1aa5596dc541b4694af584e01895c45092ac
Oleksandr <sashkooooo81@gmail.com>
2019-03-07 09:43:02 +0000

    created file

---
+++ index.js
@@ -0,0 +1,16 @@
+// task  https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript?fbclid=IwAR1_BqZS2ys8ndXwJe-l3VNzA6cqrwwQcLxtDAn65IBRveKxs-NSzO2mQb8
+// Write a function calculating circumference of a Circle.
+function circleCircumference(circle) {
+return circle = 2 * Math.PI * circle.radius;
+}
+
+//https://www.codewars.com/kata/525a5b2185a9a4f5670006c1
+let myObj = {};
+myObj.__proto__ = MyObject.prototype
+MyObject.call(myObj)
+
+//https://www.codewars.com/kata/571f1eb77e8954a812000837
+// Give you a function ```animal```, accept 1 parameter:```obj``` like this:
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
